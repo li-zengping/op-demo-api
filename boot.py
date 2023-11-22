a07a4ac4bbe6fee91bf163f3d946870905377ea2
li-zengping <40142280+li-zengping@users.noreply.github.com>
2023-11-22 22:54:28 +0800

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
-
+from flask import Flask
+from flask import request, Response
 app = Flask(__name__)
+import json
 
-@app.route('/healthy')
-def healthy():
-    is_healthy = True
-    response_data = {'status': 'healthy' if is_healthy else 'unhealthy'}
-    return jsonify(response_data)
-
+@app.route('/healthy', methods=['GET', 'POST'])
+def healthyRun():
+    if request.method == "GET":
+       parameterInfo = "healthy success"
+       client = request.remote_addr
+       return Response(json.dumps({"code": 200, "data": parameterInfo,"clientIp":client}), mimetype='application/json')
+    else:
+       parameterInfo = "请求不支持,请检查"
+       return Response(json.dumps({"code": 500, "data": parameterInfo}), mimetype='application/json')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        debug=True
+    )
